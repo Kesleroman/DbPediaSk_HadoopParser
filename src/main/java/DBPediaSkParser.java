3096d69c1427c46c7e6603597b57e07a6b18e942
Kesleroman <slonsky.roman@gmail.com>
2020-11-01 23:08:32 +0100

    Added possibility to parse the labels_sk.ttl file

--- src/main/java/DBPediaSkParser.java
+++ src/main/java/DBPediaSkParser.java
@@ -11,7 +11,6 @@ import org.apache.avro.mapreduce.AvroJob;
 import org.apache.avro.mapreduce.AvroKeyValueOutputFormat;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -25,68 +24,84 @@ import org.slf4j.LoggerFactory;
 
 public class DBPediaSkParser extends Configured implements Tool {
 
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, Text>{
 
         private static Logger logger = LoggerFactory.getLogger(TokenizerMapper.class);
-        private Text word = new Text();
+        private static Pattern idPattern = Pattern.compile("\"([0-9]+)\"\\^\\^");
+        private static Pattern labelPattern = Pattern.compile("\"(.+)\"@sk");
+        private Text returnKey = new Text();
+        private Text returnValue = new Text();
 
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
-            StringTokenizer tokenizer = new StringTokenizer(value.toString(), " ");
-            Pattern idPattern = Pattern.compile("\"([0-9]+)\"\\^\\^");
+            String valueString = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(valueString, " ");
 
-            while(tokenizer.hasMoreTokens())
-            {
-                String page = tokenizer.nextToken();
-                logger.info("Page name token: " + page);
 
-                if (!tokenizer.hasMoreTokens()) return;
-                String t = tokenizer.nextToken(); // Skip the second token
-                logger.info("Second token: " + t);
+            String page = tokenizer.nextToken();
+            logger.info("Page name token: " + page);
 
-                if (!tokenizer.hasMoreTokens()) return;
-                String idString = tokenizer.nextToken(); // The third token is id
-                logger.info("Third token: " + idString);
+            if (!tokenizer.hasMoreTokens()) return;
+            String dataType = tokenizer.nextToken();
 
-                Matcher matcher = idPattern.matcher(idString);
-                if(matcher.find())
-                {
-                    idString = matcher.group(1);
-                    logger.info("Matched id: " + idString);
-                }
-
-                int id = Integer.parseInt(idString);
-
-                word.set(page);
-                context.write(word, new IntWritable(id));
-
-                if(!tokenizer.nextToken().equals("."))
-                    logger.warn("There was not a dot in the end!");
+            Matcher matcher = getAppropriateMatcher(dataType, value.toString());
+            if (matcher == null) {
+                logger.error("Unknown data type: " + dataType);
+                return;
             }
+
+            String matchedData;
+            if(matcher.find()) {
+                matchedData = matcher.group(1);
+                logger.info("Matched data: " + matchedData);
+            }
+            else {
+                logger.warn("Nothing were matched.");
+                return;
+            }
+
+            returnKey.set(page);
+            returnValue.set(matchedData);
+            context.write(returnKey, returnValue);
+
+            if(!valueString.endsWith("."))
+                logger.warn("There was not a dot in the end!");
+        }
+
+        private Matcher getAppropriateMatcher(String dataType, String data){
+            if (dataType.equals("<http://www.w3.org/2000/01/rdf-schema#label>"))
+                return labelPattern.matcher(data);
+            else if (dataType.equals("<http://dbpedia.org/ontology/wikiPageID>"))
+                return idPattern.matcher(data);
+
+            return null;
         }
     }
 
-        public static class IdReducer extends Reducer<Text,IntWritable, AvroKey<CharSequence>, AvroValue<Integer>> {
+        public static class IdReducer extends Reducer<Text,Text, AvroKey<CharSequence>, AvroValue<CharSequence>> {
 
         private static Logger logger = LoggerFactory.getLogger(IdReducer.class);
-        private IntWritable result = new IntWritable();
 
         @Override
-        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+        public void reduce(Text key, Iterable<Text> values, Context context)
                 throws IOException, InterruptedException {
 
-            Iterator<IntWritable> itr = values.iterator();
+            Iterator<Text> itr = values.iterator();
             if (!itr.hasNext())
             {
                 logger.warn("Empty reducer!");
                 return;
             }
 
-            logger.info("Reducing: " + key);
-            result = itr.next();
+//            logger.info("Reducing: " + key);
+            StringBuilder resultBuilder = new StringBuilder();
+            while (itr.hasNext()){
+                Text text = itr.next();
+                resultBuilder.append(text.toString());
+            }
 
             AvroKey<CharSequence> avroKey = new AvroKey<CharSequence>(key.toString());
-            AvroValue<Integer> avroValue = new AvroValue<Integer>(result.get());
+            AvroValue<CharSequence> avroValue = new AvroValue<CharSequence>(resultBuilder.toString());
             context.write(avroKey, avroValue);
 
             if(itr.hasNext())
@@ -96,8 +111,8 @@ public class DBPediaSkParser extends Configured implements Tool {
 
     public int run(String[] args) throws Exception {
         if (args.length != 2) {
-            System.err.println("Usage: MapReduceColorCount <input path> <output path>");
-            return -1;
+            System.err.println("Parameters: <input path> <output path>");
+            return 1;
         }
 
         Job job = Job.getInstance(getConf(), "DbPedia parser");
@@ -108,12 +123,12 @@ public class DBPediaSkParser extends Configured implements Tool {
 
         job.setMapperClass(TokenizerMapper.class);
         job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(IntWritable.class);
+        job.setMapOutputValueClass(Text.class);
 
         job.setOutputFormatClass(AvroKeyValueOutputFormat.class);
         job.setReducerClass(IdReducer.class);
         AvroJob.setOutputKeySchema(job, Schema.create(Schema.Type.STRING));
-        AvroJob.setOutputValueSchema(job, Schema.create(Schema.Type.INT));
+        AvroJob.setOutputValueSchema(job, Schema.create(Schema.Type.STRING));
 
         return (job.waitForCompletion(true) ? 0 : 1);
     }
